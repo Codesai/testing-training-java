29eb4e5645164db7f104eac85f28c7b44805c0b5
trikitrok <trikitrok@gmail.com>
2024-09-14 13:40:03 +0200

    adding test class

---
+++ argent-rose/src/test/java/com/argentrose/ArgentRoseStoreTest.java
@@ -0,0 +1,15 @@
+package com.argentrose;
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ArgentRoseStoreTest {
+
+    @Test
+    public void fix_me_and_rename_me() {
+        assertThat(true, is(false));
+    }
+
+
+}
