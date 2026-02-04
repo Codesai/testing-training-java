f65fe42f4e21157692f2fb4d2a6e159251d68501
freyes <fran@codesai.com>
2026-02-04 16:50:05 +0000

    format

--- breaking-dependencies/problematic-searcher/src/main/java/problematic_searcher/PeopleSearcher.java
+++ breaking-dependencies/problematic-searcher/src/main/java/problematic_searcher/PeopleSearcher.java
@@ -3,8 +3,7 @@ package problematic_searcher;
 import java.util.List;
 
 public class PeopleSearcher {
-        public List<Person> search(String name)
-        {
-            return PersonsRepository.query("select * from Person where Name like '%" + name + "%'");
-        }
+    public List<Person> search(String name) {
+        return PersonsRepository.query("select * from Person where Name like '%" + name + "%'");
+    }
 }
