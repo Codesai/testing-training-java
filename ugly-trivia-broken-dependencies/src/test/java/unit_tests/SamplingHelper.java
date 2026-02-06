6d2ade8ecff7827556b47b07f2028512a8b8fef3
freyes <fran@codesai.com>
2026-02-06 16:39:30 +0000

    add sampling helper

---
+++ ugly-trivia-broken-dependencies/src/test/java/unit_tests/SamplingHelper.java
@@ -0,0 +1,16 @@
+package unit_tests;
+
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.function.Function;
+
+public class SamplingHelper {
+    public static <T> void printValues(List<T> values, Function<? super T, String> formatter) {
+        var joiner = new StringJoiner(", ");
+        values.stream()
+                .map(formatter)
+                .forEach(joiner::add);
+
+        System.out.println("Arrays.asList(" + joiner + ")");
+    }
+}
