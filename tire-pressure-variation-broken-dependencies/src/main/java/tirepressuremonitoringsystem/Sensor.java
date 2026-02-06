da65c01a83c951b36839968585a16b33a1017f99
trikitrok <trikitrok@gmail.com>
2026-02-06 19:58:04 +0100

    fixing typo

--- tire-pressure-variation-broken-dependencies/src/main/java/tirepressuremonitoringsystem/Sensor.java
+++ tire-pressure-variation-broken-dependencies/src/main/java/tirepressuremonitoringsystem/Sensor.java
@@ -13,7 +13,7 @@ public class Sensor {
     }
 
     private static double samplePressure() {
-        // placeholder implementation that simulate a real sensor in a real tire
+        // placeholder implementation that simulates a real sensor in a real tire
         Random basicRandomNumbersGenerator = new Random();
         double pressureTelemetryValue = 6 * basicRandomNumbersGenerator.nextDouble() * basicRandomNumbersGenerator.nextDouble();
         return pressureTelemetryValue;
