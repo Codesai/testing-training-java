67c4399a9c8efac806fd2f128cc872c996540a9c
trikitrok <trikitrok@gmail.com>
2026-02-06 16:12:44 +0100

    renamed constants

--- tire-pressure-variation/src/main/java/tirepressuremonitoringsystem/Alarm.java
+++ tire-pressure-variation/src/main/java/tirepressuremonitoringsystem/Alarm.java
@@ -1,17 +1,17 @@
 package tirepressuremonitoringsystem;
 
 public class Alarm {
-    private final double LowPressureThreshold = 17;
-    private final double HighPressureThreshold = 21;
 
-    private Sensor sensor = new Sensor();
+    private static final int LOW_PRESSURE_THRESHOLD = 17;
+    private static final int MAX_PRESSURE_THRESHOLD = 21;
+    private final Sensor sensor = new Sensor();
 
     private boolean alarmOn = false;
 
     public void check() {
         double psiPressureValue = sensor.popNextPressurePsiValue();
 
-        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue) {
+        if (psiPressureValue < (double) LOW_PRESSURE_THRESHOLD || (double) MAX_PRESSURE_THRESHOLD < psiPressureValue) {
             if(!isAlarmOn()) {
                 alarmOn = true;
                 System.out.println("Alarm activated!");
