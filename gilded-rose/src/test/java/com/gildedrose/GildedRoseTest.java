69c51f5bcf820416d12eedda1378b85ef0d41a35
freyes <fran@codesai.com>
2026-02-10 08:21:03 +0000

    formatting

--- gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ gilded-rose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class GildedRoseTest {
 
-  @Test
-  public void fix_me() {
-    Item[] items = new Item[]{new Item("foo", 0, 0)};
-    GildedRose app = new GildedRose(items);
+    @Test
+    public void fix_me() {
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
+        GildedRose app = new GildedRose(items);
 
-    app.updateQuality();
+        app.updateQuality();
 
-    assertEquals("fixme", app.items[0].name);
-  }
+        assertEquals("fixme", app.items[0].name);
+    }
 }
